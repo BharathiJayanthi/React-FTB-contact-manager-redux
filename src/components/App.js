50081ede97a566ae9580ab5bbc55c8fd3a24ed48
Nathaniel Stephens <ndstephens@gmail.com>
2018-12-08 14:06:04 -0500

    Setup Redux and contact reducer

--- src/components/App.js
+++ src/components/App.js
@@ -8,26 +8,31 @@ import EditContact from './EditContact'
 import About from './About'
 import NotFound from './NotFound'
 
+import { Provider } from 'react-redux'
+import store from '../store'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 // import '../App.css'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Header branding="Contact Manager" />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Contacts} />
-              <Route exact path="/contact/add" component={AddContact} />
-              <Route exact path="/contact/edit/:id" component={EditContact} />
-              <Route exact path="/about" component={About} />
-              <Route component={NotFound} />
-            </Switch>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
+                <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     )
   }
 }
