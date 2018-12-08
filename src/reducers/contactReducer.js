f95df4f36679e9df2dee54b003fec7a641d5d4ad
Nathaniel Stephens <ndstephens@gmail.com>
2018-12-08 15:27:19 -0500

    GET Request dispatch

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -2,24 +2,24 @@ import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from '../actions/types'
 
 const initialState = {
   contacts: [
-    {
-      id: 1,
-      name: 'John Doe',
-      email: 'jdoe@gmail.com',
-      phone: '555-555-5555',
-    },
-    {
-      id: 2,
-      name: 'Karen Williams',
-      email: 'karen@gmail.com',
-      phone: '222-222-2222',
-    },
-    {
-      id: 3,
-      name: 'Henry Johnson',
-      email: 'henry@gmail.com',
-      phone: '111-111-1111',
-    },
+    // {
+    //   id: 1,
+    //   name: 'John Doe',
+    //   email: 'jdoe@gmail.com',
+    //   phone: '555-555-5555',
+    // },
+    // {
+    //   id: 2,
+    //   name: 'Karen Williams',
+    //   email: 'karen@gmail.com',
+    //   phone: '222-222-2222',
+    // },
+    // {
+    //   id: 3,
+    //   name: 'Henry Johnson',
+    //   email: 'henry@gmail.com',
+    //   phone: '111-111-1111',
+    // },
   ],
 }
 
@@ -28,6 +28,7 @@ export default function(state = initialState, action) {
     case GET_CONTACTS:
       return {
         ...state,
+        contacts: action.payload,
       }
     case DELETE_CONTACT:
       return {
