04394d4f7591a3534f9ea51050ca56bfae4a8d91
Nathaniel Stephens <ndstephens@gmail.com>
2018-12-08 14:37:45 -0500

    Dispatching action

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,7 +1,34 @@
-const initialState = {}
+import { GET_CONTACTS } from '../actions/types'
+
+const initialState = {
+  contacts: [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'jdoe@gmail.com',
+      phone: '555-555-5555',
+    },
+    {
+      id: 2,
+      name: 'Karen Williams',
+      email: 'karen@gmail.com',
+      phone: '222-222-2222',
+    },
+    {
+      id: 3,
+      name: 'Henry Johnson',
+      email: 'henry@gmail.com',
+      phone: '111-111-1111',
+    },
+  ],
+}
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state,
+      }
     default:
       return state
   }
